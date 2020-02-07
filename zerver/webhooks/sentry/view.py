bcad0b2c57ad8155cf14d76d3515fb29521ac111
Chris Heald <cheald@gmail.com>
2020-02-07 02:07:21 -0700

    Improve the Sentry webhooks

--- zerver/webhooks/sentry/view.py
+++ zerver/webhooks/sentry/view.py
@@ -10,22 +10,33 @@ from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
 MESSAGE_TEMPLATE = """
-New [issue]({url}) (level: {level}):
+**[{title}]({url})**
 
-``` quote
+```quote
 {message}
 ```
+
+**Project:** {project} ({env})
+**Server:** {server}
 """.strip()
 
-@api_key_only_webhook_view('Sentry')
+@api_key_only_webhook_view('Sentry (TTI)')
 @has_request_variables
 def api_sentry_webhook(request: HttpRequest, user_profile: UserProfile,
                        payload: Dict[str, Any] = REQ(argument_type='body')) -> HttpResponse:
     subject = "{}".format(payload.get('project_name'))
+    tags = payload.get("event").get("tags", [])
+    tags_dict = {}
+    for tag in tags:
+        tags_dict[tag[0]] = tag[1]
+
     body = MESSAGE_TEMPLATE.format(
-        level=payload['level'].upper(),
-        url=payload.get('url'),
-        message=payload.get('message')
+            title=payload.get("event").get("title"),
+            url=payload.get("url"),
+            message=payload.get('message'),
+            project=payload.get("project"),
+            env=payload.get("event").get("environment"),
+            server=tags_dict.get("server_name", "unknown")
     )
 
     check_send_webhook_message(request, user_profile, subject, body)
